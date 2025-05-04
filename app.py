15f4139613a7b9c73c9a3336ad3eb3959cd743ad
Felipe Antunes <a.felipe@outlook.com>
2025-05-04 15:25:42 -0300

    refatorar: organizar funções e melhorar a estrutura do código

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-print('Sabor Express\n')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    print('Sabor Express\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolhida}')
+def exibir_menu():
+    print('Menu de opções:')
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app...')
+    exit()
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_menu()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
+    # Executa a função principal
